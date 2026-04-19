f1cf949496a0301c14962cf009eaff6f2fc14f56
Ayan-glitch-7 <mohdayan7245@gmail.com>
2026-04-19 15:55:49 +0000

    c

--- 02_vite-react/src/main.jsx
+++ 02_vite-react/src/main.jsx
@@ -1,10 +1,39 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+function MyApp() {
+  return (
+    <div>
+      <h1>CUSTOM APP !</h1>
+    </div>
+  );
+}
+
+// const reactElement = {
+//   type: "a",
+//   props: {
+//     href: "https://google.com",
+//     target: "_blank",
+//   },
+//   children: "Click me to visit google",
+// };
+
+const anotherElement = (
+  <a href="https://google.com" target='_blank'>VISIT GOOGLE</a>
 )
+
+const anotherUser = "chai aur react"
+
+const reactElement = React.createElement(
+  'a',
+  {href: 'https://google.com', target: '_blank'},
+  'click me to visit google',
+  anotherUser
+)
+
+createRoot(document.getElementById("root")).render(
+  
+    reactElement
+  
+);
