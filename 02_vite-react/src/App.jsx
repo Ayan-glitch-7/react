192df61748a45d8330e06d81a6d7bd3747326a00
Ayan-glitch-7 <mohdayan7245@gmail.com>
2026-04-19 14:08:07 +0000

    c

---
+++ 02_vite-react/src/App.jsx
@@ -0,0 +1,13 @@
+import Chai from "./Chai"
+
+function App() {
+  return (
+    <>
+    <Chai/>
+    <h1>chai aur react {username} !</h1>
+    <p>test para</p>
+    </> 
+  )
+}
+
+export default App
