636700743dd9a9573dbfaabf16dbad0805ce812d
Ayan-glitch-7 <mohdayan7245@gmail.com>
2026-08-24 22:09:28 +0530

    c

--- 02_vite_react/src/main.jsx
+++ 02_vite_react/src/main.jsx
@@ -1,4 +1,4 @@
-import { StrictMode } from "react";
+import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 
@@ -10,15 +10,32 @@ function MyApp() {
   );
 }
 
-const ReactElement = {
-  type: "a",
-  props: {
+// const ReactElement = {
+//   type: "a",
+//   props: {
+//     href: "https://google.com",
+//     target: "_blank",
+//   },
+//   children: "click me to visit google",
+// };
+
+const anotherElement = (
+  <a href="https://google.com" target="_blank">
+    visit google
+  </a>
+);
+
+const anotherUser = "chai aur react";
+
+const reactElement = React.createElement(
+  "a",
+  {
     href: "https://google.com",
-    tsrget: "_blank",
+    target: "_blank",
   },
-  children: "click me to visit google",
-};
+  "click me to visit google",
+  anotherUser,
+);
 
-
-
-createRoot(document.getElementById("root")).render(ReactElement);
+createRoot(document.getElementById("root")).render(reactElement);
+ 
