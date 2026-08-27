d3b98446f7fd6d1e1fd1889aca6ba8ab66a98bea
Ayan-glitch-7 <mohdayan7245@gmail.com>
2026-08-27 14:20:26 +0530

    c

--- 08_currency_convertor/src/hooks/useCurrencyInfo.js
+++ 08_currency_convertor/src/hooks/useCurrencyInfo.js
@@ -1,3 +1,27 @@
 import { useEffect, useState } from "react";
 
-function useCurrencyInfo
+function useCurrencyInfo(currency) {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    fetch(`https://api.frankfurter.dev/v2/rates?base=${currency}`)
+      .then((res) => res.json())
+      .then((res) => {
+        const rates = {};
+
+        res.forEach((item) => {
+          rates[item.quote] = item.rate;
+        });
+
+        setData(rates);
+      })
+      .catch((error) => {
+        console.log(error);
+        setData({});
+      });
+  }, [currency]);
+
+  return data;
+}
+
+export default useCurrencyInfo;
