3d323ed8f63bd78f39b6f065f07e4087120ce04a
Ayan-glitch-7 <mohdayan7245@gmail.com>
2026-09-08 22:21:40 +0530

    c

--- 14_mega_blog/src/components/index.js
+++ 14_mega_blog/src/components/index.js
@@ -1,4 +1,7 @@
 import Header from "./header/Header";
 import Footer from "./footer/Footer";
+import Container from "./container/Container";
+import Logo from "./Logo";
+import LogoutbBtn from "./header/LogoutbBtn";
 
-export { Header, Footer };
+export { Header, Footer, Container, Logo, LogoutbBtn };
