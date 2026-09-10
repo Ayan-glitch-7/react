2a5cee17c30709354c34da4e0b215df189a4b7df
Ayan-glitch-7 <mohdayan7245@gmail.com>
2026-09-10 19:14:02 +0530

    c

--- 14_mega_blog/src/components/index.js
+++ 14_mega_blog/src/components/index.js
@@ -3,5 +3,29 @@ import Footer from "./footer/Footer";
 import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutbBtn from "./header/LogoutbBtn";
+import RTE from "./RTE";
+import Signup from "../pages/Signup";
+import Login from "../pages/Login";
+import PostForm from "./post-form/PostForm";
+import PostCard from "./PostCard";
+import AuthLayout from "./AuthLayout";
+import Select from "./Select";
+import Input from "./Input";
+import Button from "./Button";
 
-export { Header, Footer, Container, Logo, LogoutbBtn };
+export {
+  Header,
+  Footer,
+  Container,
+  Logo,
+  LogoutbBtn,
+  RTE,
+  Signup,
+  Login,
+  PostForm,
+  PostCard,
+  AuthLayout,
+  Select,
+  Input,
+  Button,
+};
